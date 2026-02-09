c8c2dec1002b159ecf0c1840c0097de2023c7c68
antonioalvarezquevedo-boop <antonio.alvarez.quevedo@gmail.com>
2026-02-09 01:29:54 -0300

    Add navigation bar to nav.php

---
+++ includes/nav.php
@@ -0,0 +1,6 @@
+<nav class="nav-main">
+    <div class="nav-content">
+        <a href="index.php" class="nav-link">Dashboard</a>
+        <a href="bomberos.php" class="nav-link">Bomberos</a>
+    </div>
+</nav>
